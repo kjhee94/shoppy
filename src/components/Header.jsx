80d87328cc2d55ec8b61b862768905b07debed00
강주희 <kjhee94@naver.com>
2024-07-10 17:27:23 +0900

    사용자인증

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaShopify } from "react-icons/fa6";
 import { FaPencilAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { login, logout, onUserStateChange } from "../api/firebase";
+import User from "./User";
 
 export default function Header() {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange(setUser);
+  }, []);
+
   return (
     <header className="w-full h-[55px] px-4 mb-4 flex items-center justify-between">
       <Link to="/" className="flex items-center text-brand">
@@ -11,12 +19,28 @@ export default function Header() {
         <h2 className="pl-1 mt-1 text-2xl ">Shoppy</h2>
       </Link>
       <nav className="flex items-center gap-2 font-semibold">
-        <Link to="/products" className="p-2">Products</Link>
-        <Link to="/carts" className="p-2">Cart</Link>
+        <Link to="/products" className="p-2">
+          Products
+        </Link>
+        <Link to="/carts" className="p-2">
+          Cart
+        </Link>
         <Link to="/products/new" className="p-2">
           <FaPencilAlt />
         </Link>
-        <button className="font-semibold">Login</button>
+        {!user && (
+          <button
+            className="font-semibold bg-brand p-2 rounded-lg text-white"
+            onClick={login}
+          >
+            Login
+          </button>
+        )}
+        {user && (
+          <button onClick={logout}>
+            <User user={user}></User>
+          </button>
+        )}
       </nav>
     </header>
   );
